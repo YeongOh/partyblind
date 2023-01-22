4217a5a33583915ccb577d0b579650d8d4b4ddfb
Yeong <yjo0723@hotmail.com>
2023-01-22 04:36:15 -0800

    Ready to deploy

--- server/config/dbConn.js
+++ server/config/dbConn.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
 const connectDB = async () => {
+  console.log(process.env.DATABASE_URI);
   try {
     await mongoose.connect(process.env.DATABASE_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
   } catch (error) {
-    console.log(process.env.DATABASE_URI);
     console.error(error);
   }
 };
